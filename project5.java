bf84decba686c3bb6e7740cde754784330e7879e
OutbreakSource <56371605+OutbreakSource@users.noreply.github.com>
2019-11-06 11:33:25 -0700

    We in it now babbyyy

---
+++ project5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.*; 
+
+public class project5{
+
+   public static void main(String[] args) throws IOException {
+        final String INPUT_FILE = "1050_Project_05_Input.txt";
+        final String OUTPUT_FILE = "1050_Project_05_Output.txt";
+
+        // Access the input/output files
+        File inputDataFile = new File(INPUT_FILE);
+        Scanner inputFile = new Scanner(inputDataFile);
+
+        FileWriter outputDataFile = new FileWriter(OUTPUT_FILE);
+        PrintWriter outputFile = new PrintWriter(outputDataFile);
+   }
+   
+   public static void FileSection(String[] args) throws IOException{
+        final String INPUT_FILE = "1050_Project_05_Input.txt";
+        final String OUTPUT_FILE = "1050_Project_05_Output.txt";
+
+        // Access the input/output files
+        File inputDataFile = new File(INPUT_FILE);
+        Scanner inputFile = new Scanner(inputDataFile);
+
+        FileWriter outputDataFile = new FileWriter(OUTPUT_FILE);
+        PrintWriter outputFile = new PrintWriter(outputDataFile);
+   
+   }
+}
